80b091e73aa62193ed11982f4c3f149569fa8505
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-06-04 12:11:35 -0300

    created splash screen

---
+++ lib/src/splash.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          children: [
+            // Load a Lottie file from your assets
+            Lottie.asset('assets/lottie/splash_animation.json'),
+          ],
+        ),
+      ),
+    );
+  }
+}
