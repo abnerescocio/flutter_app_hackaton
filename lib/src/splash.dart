8f5d3b8b2a092361649ffcfa788beab120fb205c
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-06-04 14:24:06 -0300

    finished splash screen

--- lib/src/splash.dart
+++ lib/src/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_hackaton/src/themes/custom_colors.dart';
 import 'package:lottie/lottie.dart';
 
 void main() => runApp(const MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: CustomColors.mainBackground,
         body: Center(
           child: Lottie.asset('assets/lottie/splash_animation.json',
               width: 190, height: 231, alignment: Alignment.center,),
