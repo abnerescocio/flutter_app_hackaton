431fbd919bc0fba5371a419a5bd10424134b618d
Abner Souza <abnersouza@mobills.com.br>
2022-06-04 15:12:25 -0300

    Ajustes

--- lib/src/data/repositories.dart
+++ lib/src/data/repositories.dart
@@ -1,20 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_app_hackaton/src/data/mappers.dart';
 import 'package:flutter_app_hackaton/src/models/user.dart';
-
-import '../../firebase_options.dart';
 
 class UserRepository {
   Future<DocumentSnapshot> getById(String userId) async {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    return FirebaseFirestore.instance.collection("users").doc(userId).get();
+    return FirebaseFirestore.instance
+        .collection(User.collection)
+        .doc(userId)
+        .get();
   }
 
-  Future<void> save(User user, String userId) async {
-    final data = UserMapper.toMap(user);
-    return FirebaseFirestore.instance.collection("users").doc(userId).set(data);
+  Future<void> save(Map<String, dynamic> user, String userId) async {
+    return FirebaseFirestore.instance
+        .collection(User.collection)
+        .doc(userId)
+        .set(user);
   }
 }
