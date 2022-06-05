4ae0c49b133e2c25b51ae9461b401c72a848541f
Mateus <mateusnobref@gmail.com>
2022-06-05 09:17:33 -0300

    Added import

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,6 +6,8 @@ import 'package:flutter_app_hackaton/src/splash/splash.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'active_training/active_traning.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
