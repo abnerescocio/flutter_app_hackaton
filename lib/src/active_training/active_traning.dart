75ac813f8ab65099653e627fdf29503c899d7b82
Mateus <mateusnobref@gmail.com>
2022-06-05 14:38:33 -0300

    ANIMAATIONS OF tabata

--- lib/src/active_training/active_traning.dart
+++ lib/src/active_training/active_traning.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app_hackaton/src/commons/animated_circle_widget.dart';
@@ -9,7 +10,7 @@ import '../commons/music_buttons.dart';
 import '../themes/custom_colors.dart';
 import '../utils/converter_utils.dart';
 
-enum TrainnigStatus { rest, trainning }
+enum TrainnigStatus { rest, trainning, preparing, interval }
 
 class ActiveTrainingScreen extends StatefulWidget {
   const ActiveTrainingScreen({Key? key}) : super(key: key);
@@ -24,51 +25,158 @@ class _ActiveTrainingScreenState extends State<ActiveTrainingScreen> {
   TrainnigStatus _trainnigStatus = TrainnigStatus.trainning;
   bool _isPaused = false;
 
-  final int _trainningTime = 3;
-  final int _restingTime = 2;
-  final int _totalCyclesAmount = 3;
-  final int _totalSeriesAmount = 3;
+  final int _trainningTime = 5;
+  final int _restingTime = 5;
+  final int _totalCyclesAmount = 2;
+  final int _totalSeriesAmount = 2;
+  final int _totalCyclesInterval = 10;
 
-  int _trainningTimeCount = 10;
+  int _trainningTimeCount = 4;
   int _restingTimeCount = 5;
   int _totalTimeCount = 0;
   int _cyclesCount = 1;
   int _seriesCount = 1;
+  int _cyclesIntervalTimeCount = 10;
 
   Timer? _timer;
 
+  List getStatusViewModel(TrainnigStatus status) {
+    switch (status) {
+      case TrainnigStatus.trainning:
+        return [
+          "Exercite",
+          "weight_with_clock",
+          CustomColors.primaryPurpleDark,
+          _trainningTimeCount,
+          _trainningTime
+        ];
+
+      case TrainnigStatus.rest:
+        return [
+          "Descanse",
+          "couch-timer",
+          CustomColors.green,
+          _restingTimeCount,
+          _restingTime
+        ];
+
+      case TrainnigStatus.preparing:
+        return ["Prepare-se", "couch-timer", CustomColors.green, ""];
+      case TrainnigStatus.interval:
+        return [
+          "Intervalo",
+          "couch-timer",
+          CustomColors.green,
+          _cyclesIntervalTimeCount,
+          _totalCyclesInterval
+        ];
+    }
+  }
+
   void startTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      setState(() {
-        _totalTimeCount = _totalTimeCount + 1;
-        switch (_trainnigStatus) {
-          case TrainnigStatus.trainning:
-            if (_trainningTimeCount > 0) {
-              _trainningTimeCount = _trainningTimeCount - 1;
-            } else {
-              _trainnigStatus = TrainnigStatus.rest;
-              _trainningTimeCount = _trainningTime;
-            }
-            break;
-          case TrainnigStatus.rest:
-            if (_restingTimeCount > 0) {
-              _restingTimeCount = _restingTimeCount - 1;
-            } else {
-              _trainnigStatus = TrainnigStatus.trainning;
-              _restingTimeCount = _restingTime;
-              _seriesCount = _seriesCount + 1;
-              if (_seriesCount == _totalSeriesAmount + 1) {
-                _cyclesCount = _cyclesCount + 1;
-                _seriesCount = 1;
-                if (_cyclesCount == _totalCyclesAmount) {
-                  timer.cancel();
-                }
-              }
-            }
-            break;
-        }
-      });
+      timeStep();
+      trainningStep();
+      intervalStep();
+      restStep();
+      trainningToRest();
+      trainningToInterval();
+      trainningToFinish();
+      intervalToTrainnig();
+      restToTrainning();
     });
+  }
+
+  void timeStep() {
+    setState(() {
+      _totalTimeCount += 1;
+    });
+  }
+
+  void trainningToRest() {
+    if (_trainnigStatus == TrainnigStatus.trainning &&
+        _trainningTimeCount < 0 &&
+        _seriesCount < _totalSeriesAmount) {
+      setState(() {
+        _trainnigStatus = TrainnigStatus.rest;
+        _trainningTimeCount = _trainningTime;
+      });
+    }
+  }
+
+  void trainningToInterval() {
+    if (_trainnigStatus == TrainnigStatus.trainning &&
+        _trainningTimeCount == 0 &&
+        _seriesCount == _totalSeriesAmount &&
+        _cyclesCount < _totalCyclesAmount) {
+      setState(() {
+        _trainnigStatus = TrainnigStatus.interval;
+        _trainningTimeCount = _trainningTime;
+        _seriesCount = 1;
+        _cyclesCount += 1;
+      });
+    }
+  }
+
+  void trainningToFinish() {
+    if (_trainnigStatus == TrainnigStatus.trainning &&
+        _trainningTimeCount <= 0 &&
+        _cyclesCount >= _totalCyclesAmount &&
+        _seriesCount == _totalSeriesAmount) {
+      setState(() {
+        _trainningTimeCount = _trainningTime;
+        _timer?.cancel();
+      });
+    }
+  }
+
+  void intervalToTrainnig() {
+    if (_trainnigStatus == TrainnigStatus.interval &&
+        _cyclesIntervalTimeCount < 0) {
+      setState(() {
+        _trainnigStatus = TrainnigStatus.trainning;
+        _cyclesIntervalTimeCount = _totalCyclesInterval;
+      });
+    }
+  }
+
+  void restToTrainning() {
+    if (_trainnigStatus == TrainnigStatus.rest && _restingTimeCount < 0) {
+      setState(() {
+        _trainnigStatus = TrainnigStatus.trainning;
+        _restingTimeCount = _restingTime;
+        _seriesCount += 1;
+      });
+    }
+  }
+
+  void trainningStep() {
+    if (_trainnigStatus == TrainnigStatus.trainning) {
+      setState(() {
+        _trainningTimeCount -= 1;
+      });
+    }
+  }
+
+  void restStep() {
+    if (_trainnigStatus == TrainnigStatus.rest) {
+      setState(() {
+        _restingTimeCount -= 1;
+      });
+    }
+  }
+
+  void intervalStep() {
+    if (_trainnigStatus == TrainnigStatus.interval) {
+      setState(() {
+        _cyclesIntervalTimeCount -= 1;
+      });
+    }
+  }
+
+  double seriesProgress() {
+    return (((_cyclesCount - 1) * _totalSeriesAmount) + (_seriesCount)) /
+        (_totalCyclesAmount * _totalSeriesAmount);
   }
 
   @override
@@ -81,6 +189,8 @@ class _ActiveTrainingScreenState extends State<ActiveTrainingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var info = getStatusViewModel(_trainnigStatus);
+
     return Scaffold(
       backgroundColor: CustomColors.mainBackground,
       body: Column(
@@ -110,18 +220,14 @@ class _ActiveTrainingScreenState extends State<ActiveTrainingScreen> {
             height: 16,
           ),
           AnimatedCircleWidget(
-            firstIcon: "weight_with_clock",
-            circleColor: _trainnigStatus == TrainnigStatus.trainning
-                ? CustomColors.primaryPurple
-                : CustomColors.green,
-            titleInside: _trainnigStatus == TrainnigStatus.trainning
-                ? "$_trainningTimeCount"
-                : "$_restingTimeCount",
-            subtitleInside: _trainnigStatus == TrainnigStatus.trainning
-                ? "Exercite"
-                : "Descanse",
+            firstIcon: info[1],
+            circleColor: info[2],
+            titleInside: "${info[3]}",
+            subtitleInside: info[0],
             seriesCount: '$_seriesCount',
             seriesAmountTotal: '$_totalSeriesAmount',
+            progressValue1: info[3] / info[4],
+            progressValuer2: seriesProgress(),
           ),
           const SizedBox(
             height: 20,
@@ -138,7 +244,7 @@ class _ActiveTrainingScreenState extends State<ActiveTrainingScreen> {
               const Spacer(),
               const MusicButtons(
                 backGround: "outlined_ellipse",
-                playerIcon: "pause",
+                playerIcon: "stop",
                 width: 56,
                 height: 56,
               ),
@@ -148,7 +254,7 @@ class _ActiveTrainingScreenState extends State<ActiveTrainingScreen> {
               GestureDetector(
                 child: MusicButtons(
                   backGround: "elipse",
-                  playerIcon: _isPaused ? "pause" : "play_black",
+                  playerIcon: _isPaused ? "play_black" : "pause",
                   width: 72,
                   height: 72,
                 ),
